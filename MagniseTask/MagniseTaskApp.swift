8155bc8986f4f545c9258c2fd899048c3beeeb9e
Andrii Kravchenko <kievkao@gmail.com>
2024-11-04 09:11:55 +0200

    Initial Commit

---
+++ MagniseTask/MagniseTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagniseTaskApp.swift
+//  MagniseTask
+//
+//  Created by Andrii Kravchenko on 04.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MagniseTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
